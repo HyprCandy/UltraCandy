041679ad8cf916f6b2075b308c839c742193f998
CANDY <mirukaiano@gmail.com>
2025-09-14 21:12:46 +0300

    Add files via upload

---
+++ .config/hypr/scripts/reinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# UltraCandy Reinstall Script
+# Launches kitty in floating mode and runs the installation
+
+kitty --class="floating-installer" \
+      --override=initial_window_width=900 \
+      --override=initial_window_height=600 \
+      -e bash -c "
+git clone https://github.com/HyprCandy/ultracandyinstall.git && 
+cd ultracandyinstall && 
+bash UltraCandy_installer.sh
+"
