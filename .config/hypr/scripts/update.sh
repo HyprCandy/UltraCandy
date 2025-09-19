be28b16c30225c3a695b0b2fe1e6161e5931a215
CANDY <mirukaiano@gmail.com>
2025-09-19 20:07:59 +0300

    Update update.sh

--- .config/hypr/scripts/update.sh
+++ .config/hypr/scripts/update.sh
@@ -7,6 +7,7 @@ kitty --class="floating-installer" \
       --override=initial_window_width=900 \
       --override=initial_window_height=600 \
       -e bash -c "
+rm -rf ~/ultracandyinstall
 git clone https://github.com/HyprCandy/ultracandyinstall.git && 
 cd ultracandyinstall && 
 bash Update_UltraCandy.sh
