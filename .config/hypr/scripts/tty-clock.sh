6c3a663fd72ec1d4b75f6de7f66c685b3b8c772a
CANDY <mirukaiano@gmail.com>
2026-01-04 18:59:32 +0300

    Update tty-clock.sh

--- .config/hypr/scripts/tty-clock.sh
+++ .config/hypr/scripts/tty-clock.sh
@@ -3,7 +3,5 @@
 # TTY-Clock Script
 # Launches kitty in floating mode and runs the tty-clock
 
-kitty --class="clock" \
-      --override=initial_window_width=400 \
-      --override=initial_window_height=200 \
+kitty --app-id="clock" \
       -e bash -c "tty-clock -s -c"
