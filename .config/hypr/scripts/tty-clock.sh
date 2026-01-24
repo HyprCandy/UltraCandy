a2f575add12bab98c436efc8438c585edcf77a2a
CANDY <mirukaiano@gmail.com>
2026-01-24 04:40:26 +0300

    Add process check for tty-clock script

--- .config/hypr/scripts/tty-clock.sh
+++ .config/hypr/scripts/tty-clock.sh
@@ -2,6 +2,13 @@
 
 # TTY-Clock Script
 # Launches kitty in floating mode and runs the tty-clock
-
-kitty --app-id="clock" \
-      -e bash -c "tty-clock -s -c"
+      
+# Check if the process is running
+if pgrep -f "tty-clock -s -c" > /dev/null; then
+    # If running, kill it
+    pkill -f clock
+else
+    # If not running, start it
+    kitty --app-id="clock" \
+    	-e bash -c "tty-clock -s -c"
+fi
