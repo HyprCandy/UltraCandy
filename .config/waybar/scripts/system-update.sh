218125f2bb1836d7ec1da9ce49582ed527e9c36f
CANDY <mirukaiano@gmail.com>
2026-02-14 13:42:36 +0300

    Update system-update.sh

--- .config/waybar/scripts/system-update.sh
+++ .config/waybar/scripts/system-update.sh
@@ -41,20 +41,17 @@ export -f pkg_installed
 clean_cache() {
     echo
     print_warning "Clearing the cache frees disk space but requires redownloading if you need to downgrade later."
-    echo -e "${YELLOW}Would you like to clear the package cache? (n/Y)${NC}"
+    echo -e "${YELLOW}Would you like to clear your $aur_helper package cache? (n/Y)${NC}"
     read -r clean_choice
     case "$clean_choice" in
         [nN][oO]|[nN])
             print_status "Cache cleaning skipped."
             ;;
         *)
-            print_status "Cleaning pacman cache..."
-            # -Sc removes packages not currently installed, and old versions of installed packages
-            sudo pacman -Sc
-            
             if [ -n "$aur_helper" ]; then
-                print_status "Cleaning $aur_helper cache..."
+                print_status "󰩺 Cleaning $aur_helper cache..."
                 $aur_helper -Sc
+                print_status "󰸞 $aur_helper cache cleated."
             fi
             ;;
     esac
@@ -64,14 +61,14 @@ prompt_reboot() {
     echo
     print_warning "A reboot is recommended to ensure all changes take effect properly."
     echo
-    echo -e "${YELLOW}Would you like to reboot now? (n/Y)${NC}"
+    echo -e "${YELLOW}󰜉 Would you like to reboot now? (n/Y)${NC}"
     read -r reboot_choice
     case "$reboot_choice" in
         [nN][oO]|[nN])
             print_status "Reboot skipped. Please reboot manually when convenient."
             ;;
         *)
-            print_status "Rebooting system..."
+            print_status "󰜉 Rebooting system..."
             sudo reboot
             ;;
     esac
