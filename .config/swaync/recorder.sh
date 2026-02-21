b2c768fbbda83028cdcc74fc7c847ebb931830a7
CANDY <mirukaiano@gmail.com>
2026-02-21 13:43:14 +0300

    Adjust sleep duration in recorder script
    Increased sleep duration before starting the recorder.

--- .config/swaync/recorder.sh
+++ .config/swaync/recorder.sh
@@ -7,7 +7,7 @@ if pgrep -x "wf-recorder" > /dev/null; then
   sleep 0.1
   notify-send "Recorder" "Stopped " -t 2000
 else
-  bash -c 'wf-recorder -g -a --audio=bluez_output.78_15_2D_0D_BD_B7.1.monitor -f "$HOME/Videos/Recordings/recording-$(date +%Y%m%d-%H%M%S).mp4" $(slurp)'
-  sleep 0.1
   notify-send "Recorder" "Started " -t 2000
+  sleep 0.5
+  bash -c 'wf-recorder -g -a --audio=bluez_output.78_15_2D_0D_BD_B7.1.monitor -f "$HOME/Videos/Recordings/recording-$(date +%Y%m%d-%H%M%S).mp4" $(slurp)'
 fi
