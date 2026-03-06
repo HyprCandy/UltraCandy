e4035fb094cc36709af42431719c8ae0fcf856c0
AstralDesigns <ianmiruka1@gmail.com>
2026-03-06 21:34:12 +0300

    tweaks

--- GJS/toggle-control-center.sh
+++ GJS/toggle-control-center.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Toggle Candy Utils - Fast launch (daemon stays running)
+# No killing - daemon persists for instant widget launches
+
 PID_FILE="$HOME/.cache/hyprcandy/pids/candy-daemon.pid"
 DAEMON_SCRIPT="$HOME/.ultracandy/GJS/candy-daemon.js"
 TOGGLE_DIR="$HOME/.cache/hyprcandy/toggle"
--- GJS/toggle-media-player.sh
+++ GJS/toggle-media-player.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Toggle Media Player - Fast launch (daemon stays running)
+
 PID_FILE="$HOME/.cache/hyprcandy/pids/candy-daemon.pid"
 DAEMON_SCRIPT="$HOME/.ultracandy/GJS/candy-daemon.js"
 TOGGLE_DIR="$HOME/.cache/hyprcandy/toggle"
